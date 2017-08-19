1898d9d05ae28fff93130e4268de0c334c4a92fe
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-08-19 10:41:45 +0400

    angular seed project

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HelloComponent} from './hello/hello.component';
+
+@NgModule({
+    imports: [CommonModule],
+    providers: [],
+    declarations: [HelloComponent],
+    exports: [HelloComponent]
+})
+export class AppModule {
+
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export {AppModule} from './app/app.module';
+export {HelloComponent} from './app/hello/hello.component';
